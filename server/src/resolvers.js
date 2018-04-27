d13037b29ca8bf0a4501355b246533d853ce192b
Unknown <lucasmpinheiro@gmail.com>
2018-04-27 17:35:43 -0300

    Implement server-side offset pagination for products.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,8 +2,15 @@ import Product from './models/Product';
 
 export const resolvers = {
     Query: {
-        products: (root, { filter }) => Product.find({
-            name: { '$regex': filter, '$options': 'i' },
-        }).exec(),
+        products: async (root, { filter = '', limit = 25, skip = 0 }) => {
+            const conditions = {
+                name: { '$regex': filter, '$options': 'i' },
+            };
+
+            return {
+                totalCount: await Product.find(conditions).count(),
+                edges: await Product.find(conditions).skip(skip).limit(limit).exec(),
+            };
+        },
     },
 };
